eabdbb39323647f827c38aebc5e621b34f0fc12c
David Stanton <davidthomasstanton@gmail.com>
2024-01-09 13:29:42 -0800

    Files for Day 1

---
+++ Day_01.playground/Contents.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+var playerName = "Roy"
+print(playerName)
+playerName = "Danny"
+print(playerName)
+playerName = "Sam"
+print(playerName)
+
+
+// Strings
+
+// Multiline Strings
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+let movieLength = movie.count
+
+print(movie.uppercased())
+
+let score = 10
+let reallyBig = 100_000_000
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubleScore = score * 2
+let squareScore = score * score
+
+var counter = 10
+counter = counter + 5
+counter += 5
+
+let number = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+print(c)
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var rating = 5.0
+rating *= 2
+
+print(rating)
+
+
