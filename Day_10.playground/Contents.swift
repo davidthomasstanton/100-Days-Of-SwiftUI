e1b5d397d9f5414e20018c9cf923453faf7c87b0
David Stanton <davidthomasstanton@gmail.com>
2024-01-15 22:55:15 -0800

    Work for Day_09

---
+++ Day_10.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+// Errors, and Do, Try, Catch
+// Function that takes in a password and throws errors if the password is too short or too long
+
+
+
+
+// Closure
+// Function that takes an array of numbers and sorts them, filters out even numbers, prints each as a "lucky number"
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
