84e331b5231248200080c2c8016ac0dd19b3064b
David Stanton <davidthomasstanton@gmail.com>
2024-02-05 21:40:26 -0800

    Work for Day_32

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by David Stanton on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
