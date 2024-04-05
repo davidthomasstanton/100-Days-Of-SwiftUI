456dbdaa60fcd43e8b4dd02f3904bd0fd92b6a4e
David Stanton <davidthomasstanton@gmail.com>
2024-04-04 20:31:02 -0700

    Work for Day_66

---
+++ Instafilter/Instafilter_5/Instafilter_5/Instafilter_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Instafilter_5App.swift
+//  Instafilter_5
+//
+//  Created by David Stanton on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Instafilter_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
