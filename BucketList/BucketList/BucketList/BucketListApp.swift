590342abbcd7afc317b17f7e3bb62dee94c07139
David Stanton <davidthomasstanton@gmail.com>
2024-04-05 19:10:46 -0700

    Work for Day 67 & 68

---
+++ BucketList/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,18 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by David Stanton on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            //DocumentsDirectoryView_2()
+        }
+    }
+}
---
+++ BucketList/BucketList/BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by David Stanton on 4/5/24.
+//
+// Create a button that converts a message into Data
+// writes it into the documentsDirectory then reads it out
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("HeyO, World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
