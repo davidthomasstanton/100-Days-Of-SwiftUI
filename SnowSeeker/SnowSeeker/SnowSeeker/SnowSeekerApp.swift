855d093f36380b91ada859de1d9dd783130c4bed
David Stanton <davidthomasstanton@gmail.com>
2024-05-14 22:00:24 -0700

    Work for Day_96

---
+++ SnowSeeker/SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,18 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by David Stanton on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+//            Searchable_1()
+        }
+    }
+}
